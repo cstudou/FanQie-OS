1c2f8d634ef5b48e1d9a8fe6c2749910a5353eb0
tudou <tudou@macbook.local>
2020-11-21 15:18:14 +0800

    os

--- start.sh
+++ start.sh
@@ -2,8 +2,8 @@ rm -rf *.bin
 rm -rf *.lock
 rm -rf *.img
 bximage -hd=10 -mode=create -sectsize=512 -q hd3M.img
-nasm -I include -o mbr.bin mbr.S
-nasm -I include -o loader.bin loader.S
+nasm -I ./boot/include -o mbr.bin ./boot/mbr.S
+nasm -I ./boot/include -o loader.bin ./boot/loader.S
 dd if=./mbr.bin of=./hd3M.img bs=512 count=1 conv=notrunc
-dd if=./loader.bin of=./hd3M.img bs=512 count=2 seek=2 conv=notrunc
+dd if=./loader.bin of=./hd3M.img bs=512 count=4 seek=2 conv=notrunc
 rm -rf *.bin
