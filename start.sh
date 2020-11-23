5ebfffaa01aee667a6253d20523d039066e7cab4
hnutudou <gyd@live.cn>
2020-11-23 20:15:37 +0800

    os

--- start.sh
+++ start.sh
@@ -5,5 +5,5 @@ bximage -hd=10 -mode=create -sectsize=512 -q hd3M.img
 nasm -I ./boot/include -o mbr.bin ./boot/mbr.S
 nasm -I ./boot/include -o loader.bin ./boot/loader.S
 dd if=./mbr.bin of=./hd3M.img bs=512 count=1 conv=notrunc
-dd if=./loader.bin of=./hd3M.img bs=512 count=4 seek=2 conv=notrunc
+dd if=./loader.bin of=./hd3M.img bs=512 count=200 seek=9 conv=notrunc
 rm -rf *.bin
