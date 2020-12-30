28ebb1899cd83cdc52f9d05c6afbb4c2ccbf0d1a
hnutudou <gyd@live.cn>
2020-12-30 21:08:14 +0800

    disk

---
+++ .history/kernel/init_20201224141134.h
@@ -0,0 +1,14 @@
+#ifndef __FANQIE_INIT_H__
+#define __FANQIE_INIT_H__
+
+#include "interrupt.h"
+#include "../device/timer.h"
+#include "../lib/kernel/memory.h"
+#include "../kernel/thread.h"
+#include "../device/console.h"
+#include "../device/keyboard.h"
+#include "tss.h"
+#include "syscall.h"
+void init_all();
+
+#endif
---
+++ .history/kernel/init_20201224141140.c
@@ -0,0 +1,13 @@
+#include "init.h"
+void init_all()
+{
+    Puts("init all!\n");
+    idt_init();
+    timer_init();
+    memory_init();
+    thread_init();
+    console_init();
+    keyboard_init();
+    tss_init();
+    syscall_init();
+}
